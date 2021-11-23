b72c96e9297df76e161d6327323f1115c964e7e2
Reliii <rensvanderlit@gmail.com>
2021-11-23 09:44:56 +0100

    j

---
+++ Assets/Code/BackgroundScrolling.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float bgSpeed;
+
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(bgSpeed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x >= 10)
+        {
+            transform.position = new Vector3(-10, (float)2.5, 0);
+        }
+    }
+}
