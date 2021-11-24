de84ab1423d5cf27f54914d99a76ed5655f76f46
Tycho van <TychovanEngelenburg@gmail.com>
2021-11-24 12:23:55 +0100

    pushy pushy

--- Assets/Code/Enemy/DestroyOnExit.cs
+++ Assets/Code/Enemy/DestroyOnExit.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnExit : MonoBehaviour
@@ -5,8 +7,23 @@ public class DestroyOnExit : MonoBehaviour
 
     // Temporary script to destroy waves when they fully exit vision. Can be replaced with ObjectPool
 
-    void OnBecameInvisible()
+    [Tooltip("The number of seconds after which this item will destroy itself.")]
+    [SerializeField] private float _lifeTime = 30.0f;
+
+    private float _deathTimer;
+
+    private void Start()
     {
-        Destroy(gameObject);
+        _deathTimer = 0.0f;
+    }
+
+    void Update()
+    {
+        // Add time to the timer, until it hits the time set in the inspector. Then send the object back to the pool.
+        _deathTimer += Time.deltaTime;
+        if (_deathTimer >= _lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
